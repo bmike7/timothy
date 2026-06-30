a5139b61a7e95d252b0d0388199204d8796de354
Mike Bijl <bijlmike3@gmail.com>
2026-06-30 10:50:33 +0200

    feat(local-db): implemented to spin up a testcontainer until an interrupt signal sent

--- timothy/cli/__init__.py
+++ timothy/cli/__init__.py
@@ -7,8 +7,11 @@ Some commands are designed for use with Git worktrees.
 
 import importlib
 import pkgutil
-from contextlib import suppress
+import signal
+import threading
+from contextlib import contextmanager, suppress
 from pathlib import Path
+from typing import Iterator
 
 import click
 
@@ -23,6 +26,21 @@ def load_commands() -> None:
         if mod.name.endswith("commands"):
             with suppress(ImportError):
                 importlib.import_module(mod.name)
+
+
+@contextmanager
+def stop_event() -> Iterator[threading.Event]:
+    event = threading.Event()
+    # Gracefully shutdown when CLI is used:
+    # - by a user
+    old_int = signal.signal(signal.SIGINT, lambda *_: event.set())
+    # - by docker, script, CI/CD, ...
+    old_term = signal.signal(signal.SIGTERM, lambda *_: event.set())
+    try:
+        yield event
+    finally:
+        signal.signal(signal.SIGINT, old_int)
+        signal.signal(signal.SIGTERM, old_term)
 
 
 def main() -> None:
--- timothy/cli/local_db/commands.py
+++ timothy/cli/local_db/commands.py
@@ -2,13 +2,45 @@
 Automates a local development workflow using Testcontainers and Git worktrees.
 """
 
+from dataclasses import dataclass
+from functools import partial, update_wrapper
+from pathlib import Path
+from typing import Callable
+
 import click
 from testcontainers.postgres import PostgresContainer
-from timothy.cli import cli
+from timothy.cli import cli, stop_event
+
+
+@dataclass
+class Cfg:
+    data_loc: Path
+
+
+def pass_ctx(f: Callable, loc: str):
+    @click.pass_context
+    def _inject_config(ctx, *args, **kwargs):
+        return ctx.invoke(f, ctx.obj[loc], *args, **kwargs)
+
+    return update_wrapper(_inject_config, f)
+
+
+pass_cfg = partial(pass_ctx, loc="cfg")
 
 
 @cli.group(help=__doc__)
-def local_db() -> None: ...
+@click.option(
+    "--data-loc",
+    type=click.Path(path_type=Path),
+    default=None,
+    help="Location of the DB data",
+)
+@click.pass_context
+def local_db(ctx, data_loc: Path | None) -> None:
+    ctx.ensure_object(dict)
+    if data_loc is None:
+        data_loc = Path.cwd() / ".db-data"
+    ctx.obj["cfg"] = Cfg(data_loc)
 
 
 @local_db.command(
@@ -18,7 +50,8 @@ def local_db() -> None: ...
     """,
 )
 @click.argument("conn_str")
-def dump(conn_str: str) -> None: ...
+@pass_cfg
+def dump(cfg: Cfg, conn_str: str) -> None: ...
 
 
 @local_db.command(
@@ -27,9 +60,22 @@ def dump(conn_str: str) -> None: ...
     that can be mounted into a local container.
     """,
 )
-def copy() -> None: ...
+@pass_cfg
+def copy(cfg: Cfg) -> None: ...
 
 
 @local_db.command(help="Spin up a Postgres Testcontainer with a local dump mounted.")
 @click.option("--version", type=click.IntRange(16, 18), default=17)
-def up(version) -> None: ...
+@pass_cfg
+def up(cfg: Cfg, version) -> None:
+    with (
+        stop_event() as event,
+        PostgresContainer(f"postgres:{version}").with_volume_mapping(
+            cfg.data_loc,
+            "/docker-entrypoint-initdb.d/seed.sql",
+            mode="ro",
+        ) as pg,
+    ):
+        click.echo(f"Postgres is running at: {pg.get_connection_url()}")
+        click.echo("Press Ctrl+C to stop.")
+        event.wait()
